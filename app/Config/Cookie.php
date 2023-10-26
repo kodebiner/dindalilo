02730f029b17f5008c32b16af11efb7b2805d15f
Kosmas Mahendra <blind_cozy@yahoo.com.sg>
2023-10-26 22:40:34 +0700

    Update Cookie.php

--- app/Config/Cookie.php
+++ app/Config/Cookie.php
@@ -45,7 +45,7 @@ class Cookie extends BaseConfig
      *
      * Set to `.your-domain.com` for site-wide cookies.
      */
-    public string $domain = 'dindalillo.binary111.com';
+    public string $domain = '';
 
     /**
      * --------------------------------------------------------------------------
