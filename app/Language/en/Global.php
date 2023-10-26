98a29fe5f211ae5773dacc2ec4b9e07b934690cf
Kosmas Mahendra <blind_cozy@yahoo.com.sg>
2023-10-26 11:53:19 +0700

    update

--- app/Language/en/Global.php
+++ app/Language/en/Global.php
@@ -6,4 +6,8 @@
  */
 return [
     'forgotPassword'    => 'Forgot Password',
+    'activation1'       => 'This is activation email for your account on',
+    'activation2'       => 'To activate your account use this URL.',
+    'activation3'       => 'If you did not registered on this website, you can safely ignore this email.',
+    'emailNotes'        => '* This email sent automatically. Do not reply to this email.',
 ];
--- app/Language/id/Global.php
+++ app/Language/id/Global.php
@@ -6,4 +6,8 @@
  */
 return [
     'forgotPassword'    => 'Lupa Password',
+    'activation1'       => 'Ini merupakan email aktivasi untuk akun kamu di',
+    'activation2'       => 'Untuk aktivasi akun kamu silahkan klik link berikut ini, atau salin dan tempel ke browser andalanmu.',
+    'activation3'       => 'Bila kamu tidak merasa mendaftarkan diri pada website ini, kamu bisa mengabaikan email ini.',
+    'emailNotes'        => '* Email ini dikirim secara otomatis. Jangan membalas email ini.',
 ];
